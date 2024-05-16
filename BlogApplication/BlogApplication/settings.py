e4da1a77655258f28fe2f04dfdae275311d67c3b
Rajneesh <rajneeshthakur26@gmail.com>
2024-05-17 00:13:02 +0530

    added a basic template

--- BlogApplication/BlogApplication/settings.py
+++ BlogApplication/BlogApplication/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = Path.joinpath(BASE_DIR,'templates')
+print(TEMPLATE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'BlogApplication.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
